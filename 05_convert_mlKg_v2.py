510c1c0b84b798cb95719bfddfdc0e0a99a42d2e
jessh123456 <83625253+jessh123456@users.noreply.github.com>
2022-06-01 17:52:46 +1200

    component 5

--- 05_convert_mlKg_v2.py
+++ 05_convert_mlKg_v2.py
@@ -3,14 +3,14 @@ This is just minor component designed to be added into 04_unit_separator_v3.
 ----- V2:
 I realized that i shouldn't add "g" and "L" onto the end because I will need
 the values separated when calculating the cheapest and most expensive option
-in the future.
+in the future. I have also made kg and ml constants.
 """
 
 # ***** Main routine *****
 # --- Units ---
 unit = ""  # this input is just for testing purposes - will be blank
-kg = 1000  # conversion rates
-ml = 0.001
+KG = 1000  # conversion rates - CONSTANTS
+ML = 0.001
 amount = 0  # these values are just for testing purposes
 amount_list = []  # list to store converted rates
 
@@ -23,9 +23,9 @@ elif unit == "ml" or "l":
     g_or_l = "l"
 
 if unit == "kg":  # if kg, convert to g
-    amount = amount*1000
+    amount = amount*KG
 elif unit == "ml":  # if mL, convert to L
-    amount = amount*0.001
+    amount = amount*ML
 
 amount_list.append(amount)  # add to list
 print(amount_list)  # for testing purposes
